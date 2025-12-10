755fd28cb3b8b9c25e8f70c9aba340e37ccb1a98
fauzan aditama <fauzanaditama123@gmail.com>
2025-12-10 16:18:25 +0700

    Create poliklinik.cpp

---
+++ poliklinik.cpp
@@ -0,0 +1,86 @@
+void createListPoli(ListPoli &LP) {
+    LP.first = nullptr;
+    LP.last = nullptr;
+}
+
+adrPoli createPoli(string nama) {
+    adrPoli P = new Poliklinik;
+    P->nama = nama;
+    P->firstDokter = nullptr;
+    P->next = nullptr;
+    P->prev = nullptr;
+
+    return P;
+}
+
+void insertPoli(ListPoli &LP, adrPoli P) {
+    if (LP.first == nullptr) {
+        LP.first = LP.last = P;
+    } else {
+        LP.last->next = P;
+        P->prev = LP.last;
+        LP.last = P;
+    }
+}
+
+adrPoli findPoli(ListPoli LP, string nama) {
+    adrPoli P = LP.first;
+    while (P != nullptr) {
+        if (P->nama == nama) {
+        return P;
+        P = P->next;
+    }
+    return nullptr;
+}
+
+void deletePoli(ListPoli &LP, string nama) {
+    adrPoli P = findPoli(LP, nama);
+    if (P == nullptr);
+
+    adrDokter D = P->firstDokter;
+    while (D != NULL) {
+        adrDokter temp = D;
+        D = D->next;
+        delete temp;
+    }
+
+    if (P == LP.first && P == LP.last) {
+        LP.first = LP.last = NULL;
+    } else if (P == LP.first) {
+        LP.first = P->next;
+        LP.first->prev = NULL;
+    } else if (P == LP.last) {
+        LP.last = P->prev;
+        LP.last->next = NULL;
+    } else {
+        P->prev->next = P->next;
+        P->next->prev = P->prev;
+    }
+
+    delete P;
+}
+
+void showAll(ListPoli LP) {
+    adrPoli P = LP.first;
+    while (P != nullptr) {
+        cout << "\nPoliklinik: " << P->nama << endl;
+        showDokter(P);
+        P = P->next;
+    }
+}
+
+adrPoli poliDokterTerbanyak(ListPoli LP) {
+    int max = -1;
+    adrPoli P = LP.first;
+    adrPoli maxPoli = NULL;
+
+    while (P != NULL) {
+        int jumlah = countDokter(P);
+        if (jumlah > max) {
+            max = jumlah;
+            maxPoli = P;
+        }
+        P = P->next;
+    }
+    return maxPoli;
+}
