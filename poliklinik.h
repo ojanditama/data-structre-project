bf9bcaf2757cfb56a2c55eda870c457c9997128e
fauzan aditama <fauzanaditama123@gmail.com>
2025-12-10 16:17:53 +0700

    poliklinik.h

--- poliklinik.h
+++ poliklinik.h
@@ -1,1 +1,27 @@
+#ifndef POLIKLINIK_H_INCLUDED
+#define POLIKLINIK_H_INCLUDED
+
+typedef Poliklinik* adrPoli;
+
+struct Poliklinik {
+    string nama;
+    adrDokter firstDokter;
+    Poliklinik next;
+    Poliklinik prev;
+};
+
+struct ListPoli {
+    adrPoli first;
+    adrPoli last;
+};
+
+void createListPoli(ListPoli &LP);
+adrPoli createPoli(string nama);
+void insertPoli(ListPoli &LP, adrPoli P);
+adrPoli findPoli(ListPoli LP, string nama);
+void deletePoli(ListPoli &LP, string nama);
+adrPoli poliDokterTerbanyak(ListPoli LP);
+void showAll(ListPoli LP);
+
+#endif // POLIKLINIK_H_INCLUDED
 
